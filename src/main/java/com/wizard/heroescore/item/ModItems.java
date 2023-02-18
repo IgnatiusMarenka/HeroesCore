7390395f090a9eaa0d73df0adf0b00386ed8e080
IgnatiusMarenka <ignatiusmarenkamarechal@gmail.com>
2023-02-18 09:39:25 +0100

    Added Some Tags and the Heroes Tablet (WIP).

--- src/main/java/com/wizard/heroescore/item/ModItems.java
+++ src/main/java/com/wizard/heroescore/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.wizard.heroescore.item;
 
 import com.wizard.heroescore.HeroesCore;
+import com.wizard.heroescore.item.custom.HeroesTabletItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -20,6 +21,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> STEEL_NUGGET = ITEMS.register("steel_nugget",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.HEROES_CORE_TAB)));
+
+    public static final RegistryObject<Item> HEROES_TABLET = ITEMS.register("heroes_tablet",
+            () -> new HeroesTabletItem(new Item.Properties().tab(ModCreativeModeTab.HEROES_CORE_TAB).durability(3)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/wizard/heroescore/item/custom/HeroesTabletItem.java
@@ -0,0 +1,65 @@
+package com.wizard.heroescore.item.custom;
+
+import com.wizard.heroescore.block.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Random;
+
+public class HeroesTabletItem extends Item {
+    public HeroesTabletItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if(!pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                Block blockBelow = pContext.getLevel().getBlockState(positionClicked.below(i)).getBlock();
+
+                if(isValuableBlock(blockBelow)) {
+                    outputValuableCoordinates(positionClicked.below(i), player, blockBelow);
+                    foundBlock = true;
+                    break;
+                }
+            }
+
+            if(!foundBlock) {
+                player.sendMessage(new TranslatableComponent("item.heroescore.heroes_tablet.no_valuables"),
+                        player.getUUID());
+            }
+        }
+
+        pContext.getItemInHand().hurt(1, new Random(),
+                pContext.getLevel().getServer().getPlayerList().getPlayer(pContext.getPlayer().getUUID()));
+
+
+        return super.useOn(pContext);
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, Player player, Block blockbelow) {
+        player.sendMessage(new TextComponent("Found " + blockbelow.asItem().getRegistryName().toString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), player.getUUID());
+    }
+
+    private boolean isValuableBlock(Block block) {
+        return block == Blocks.COPPER_ORE || block == Blocks.COAL_ORE
+                || block == Blocks.IRON_ORE || block == Blocks.DIAMOND_ORE;
+    }
+}
